1cf9d02707208ae54c2a9441590da04e4b07fc67
Fernando Nerio Jr <91402801+JrNerio30@users.noreply.github.com>
2024-03-13 23:48:56 -0600

    feat: add toggle active event and add fixed background

--- src/main.js
+++ src/main.js
@@ -2,18 +2,31 @@ const changeImagebtn = document.getElementById("change-image");
 const backgroundImg = document.querySelector(".hero");
 const imageUrls = [
   "./assets/herosecondchair.jpg",
+  "./assets/herochair.jpg",
   "./assets/brownfurniture.jpg",
-  "./assets/herochair.jpg"
 ];
 let currentIndex = 0;
 
 changeImagebtn.addEventListener("click", () => {
 
-  backgroundImg.style.background = `url(${imageUrls[currentIndex]}) center/cover no-repeat`
+  backgroundImg.style.background = `url(${imageUrls[currentIndex]}) center/cover no-repeat fixed`
 
-  currentIndex++;
+    currentIndex++;
 
-  if(currentIndex >= imageUrls.length){
-    currentIndex = 0;
+    if(currentIndex >= imageUrls.length){
+      currentIndex = 0;
+    }
   }
-});
+)
+
+
+const acc = document.getElementsByClassName("content");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("mouseout", function() {
+    this.classList.toggle("active");
+    }
+  )
+}
+
